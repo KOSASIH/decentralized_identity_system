2977a953f70a71f99e35139787db3839e4f72b7a
KOSASIH <kosasihg88@gmail.com>
2024-04-28 11:05:18 +0700

    Create service-verification.js

---
+++ service/service-verification.js
@@ -0,0 +1,12 @@
+class VerificationService {
+  constructor() {
+    // Implement verification logic here
+  }
+
+  async verify(did, data) {
+    // Verify data using did's public key
+    // Return true if verified, false otherwise
+  }
+}
+
+module.exports = VerificationService;
