dbe7fd2d96eb6125120adcd7b7c2d79558e662b3
KOSASIH <kosasihg88@gmail.com>
2024-04-28 11:04:39 +0700

    Create service-authz.js

---
+++ service/service-authz.js
@@ -0,0 +1,12 @@
+class AuthorizationService {
+  constructor() {
+    // Implement authorization logic here
+  }
+
+  async authorize(did, resource, action) {
+    // Check if did has permission to perform action on resource
+    // Return true if authorized, false otherwise
+  }
+}
+
+module.exports = AuthorizationService;
